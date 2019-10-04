54f9cd2507b991388d40758fa57362909665e42d
Charles Ponti <cj@ponti.io>
2019-10-04 13:22:42 -0400

    Add pycharm settings & jupyter notebook

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from json import JSONEncoder
 from csv import DictReader, DictWriter
 import pandas as pd
+import numpy as np
 
-person = pd.DataFrame({ 'name': 'Chase', 'age': 33, 'hair': 'black'})
+person = pd.DataFrame({
+  'name': ['Chase', 'Lucy'],
+  'age': [33, 34],
+  'hair': ['black', 'brown']
+})
+
+
+print(person)
