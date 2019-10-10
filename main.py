37ff0915283ce6ff8bc0cd4b059731c9c31b4810
Charles Ponti <cj@ponti.io>
2019-10-10 08:39:14 -0400

    Add flask server stuff

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+
+# year = [1950, 1970, 1990, 2010]
+# population = [2.3, 4.1, 5.9, 7.1]
+
+# plt.scatter(year, population)
+
+# plt.show()
 import pandas as pd
+import csv
+import json
+from flask import Flask, Request, Response
 
-transactions = pd.read_csv('./data/transactions.csv', delimiter=',')
-
-def get_amount_date_tuple(transaction):
-    print(type(transaction))
-    return transaction['Amount'], transaction['Date']
+app = Flask(__name__, static_folder='./app/build')
 
 
-amount_dates = transactions.applymap(
-  get_amount_date_tuple
-)
 
-print(amount_dates)
-year = [1950, 1970, 1990, 2010]
-population = [2.3, 4.1, 5.9, 7.1]
+# text = open('./data/transactions.csv', 'r')
+transactions = pd.read_csv('./data/transactions.csv', delimiter=',', header=0)
 
-plt.scatter(year, population)
+@app.route('/')
+def get(**args):
+  return transactions.to_json()
 
-plt.show()
+@app.route('/transactions')
+def get_by_amount():
+  return transactions.query(f"Amount > {args}")
+
+if __name__ == "__main__":
+  app.run(host='localhost', port='5000', debug=True)
+
