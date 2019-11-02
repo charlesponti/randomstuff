99688635705d5d9e7bd674eb70be427455775730
ChasePonti <cj@ponti.io>
2019-11-01 21:21:21 -0500

    update db

--- ideas/running_message/db.py
+++ ideas/running_message/db.py
@@ -1,10 +1,16 @@
+class DuplicateTableError(ValueError):
+    def __init__(self, ofcorrecttype):
+        super().__init__(ofcorrecttype)
 
 class Database():
     def __init__(self, *args, **kwargs):
         self.tables = {}
 
     def add_table(self, name: str):
-        self.tables[name] = []
+        if self.tables[name] is None:
+            self.tables[name] = []
+        else:
+            raise DuplicateTableError
 
     def add_to_table(self, name: str, record):
         self.tables[name].append(record)
