3e9d0243c9670f1ddedc373521fc82d05af2c823
Charles Ponti <cj@ponti.io>
2020-01-18 11:26:24 -0500

    add the things

--- lessons/dates.py
+++ lessons/dates.py
@@ -2,26 +2,41 @@
 # Example file for working with date information
 #
 from datetime import date
-import datetime
+from datetime import time
+from datetime import datetime
+
 
 def main():
-  ## DATE OBJECTS
-  # Get today's date from the simple today() method from the date class
-  print(date.today())
+    ## DATE OBJECTS
+    # Get today's date from the simple today() method from the date class
+    today = date.today()
+    print(today)
 
-  # print out the date's individual components
-  today = date.today()
-  print(today.year, today.month, today.day)
+    # print out the date's individual components
+    print(today.year, today.month, today.day)
 
-  # retrieve today's weekday (0=Monday, 6=Sunday)
-  print(today.weekday())
+    # retrieve today's weekday (0=Monday, 6=Sunday)
+    weekday = today.weekday()
+    days = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ]
+    print("Today is", days[today.weekday()], "and it's weekday number is", weekday)
 
-  ## DATETIME OBJECTS
-  # Get today's date from the datetime class
-  print(datetime.date.today())
+    ## DATETIME OBJECTS
+    # Get today's date from the datetime class
+    now = datetime.now()
+    print("The current date and time is", now)
 
-  # Get the current time
-  print()
+    # Get the current time
+    t = datetime.time(datetime.now())
+    print("The time is", t)
+
 
 if __name__ == "__main__":
-  main();
+    main()
