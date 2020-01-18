0f8bd4ba2c1c8fa465e44787bf60081b2a0b2c80
Charles Ponti <cj@ponti.io>
2020-01-18 11:25:38 -0500

    change the things

--- tests/test--get-divisibles.py
+++ tests/test--get-divisibles.py
@@ -1,37 +1,43 @@
 import getopt, sys
 
-# read commandline arguments, first
-fullCmdArguments = sys.argv
+def get_arguments():
+  # Get list of arguments passed to Python command
+  fullCmdArguments = sys.argv
 
-# - further arguments
-argumentList = fullCmdArguments[1:]
+  # Return only arguments with name of Python file
+  return fullCmdArguments[1:]
 
-unixOptions = ["hvs:"]
-gnuOptions = ["help", "start", "end", "verbose"]
+def get_args_with_getopt():
+  unixOptions = ["hH:sS:v"]
+  gnuOptions = ["help", "start=", "end=", "verbose"]
 
-try:
-  arguments, values = getopt.getopt(argumentList, unixOptions, gnuOptions)
-except getopt.error as err:
-  print(str(err))
-  sys.exit(2)
+  try:
+    options, arguments = getopt.getopt(get_arguments(), unixOptions, gnuOptions)
+  except getopt.error as err:
+    print(str(err))
+    sys.exit(2)
 
-for currentArgument, currentValue in arguments:
+  return options, arguments
+
+options, arguments = get_args_with_getopt()
+print(options, arguments)
+for currentArgument in arguments:
     if currentArgument in ("-v", "--verbose"):
         print ("enabling verbose mode")
     elif currentArgument in ("-h", "--help"):
         print ("displaying help")
-    elif currentArgument in ("-s", "--start"):
-        print(("will start range at (%s)") % (currentValue))
-    elif currentArgument in ("-e", "--end"):
-        print (("will end range at (%s)") % (currentValue))
+    # elif currentArgument in ("-s", "--start"):
+        # print(("will start range at (%s)") % (currentValue))
+    # elif currentArgument in ("-e", "--end"):
+        # print (("will end range at (%s)") % (currentValue))
 
-for x in range(1, int(sys.argv[1]) + 1):
-  if x % 2 == 0:
-    if x % 3 == 0:
-      print(f"{x} divisible by 2 and 3")
-    else:
-      print(f"{x} divisible by 2 and not by 3")
-  elif x % 3 == 0:
-    print(f"{x} divisible by 3 and not by 2")
-  else:
-    print(f"{x} not divisible by 2 or 3")
+# for x in range(1, int(sys.argv[1]) + 1):
+#   if x % 2 == 0:
+#     if x % 3 == 0:
+#       print(f"{x} divisible by 2 and 3")
+#     else:
+#       print(f"{x} divisible by 2 and not by 3")
+#   elif x % 3 == 0:
+#     print(f"{x} divisible by 3 and not by 2")
+#   else:
+#     print(f"{x} not divisible by 2 or 3")
