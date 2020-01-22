2705734fcfa7f78c9e775640ce0a31cfd0260ff0
Charles Ponti <cj@ponti.io>
2020-01-21 21:54:04 -0500

    refactor perf_counter

---
+++ lessons/perf_counter.py
@@ -0,0 +1,32 @@
+"""Measuring time"""
+
+from time import perf_counter
+
+def upto_for(range_of_numbers: int):
+  """Sum 1...n with a for loop"""
+  total = 0
+  for i in range(range_of_numbers):
+    total += i
+  return total
+
+def upto_sum(length_of_range):
+  return sum(range(length_of_range))
+
+def get_performance(starttime: float) -> float:
+  """Get performance relative to starttime"""
+  return perf_counter() - starttime
+
+def get_functon_performance(*args, **kwargs):
+  # Capture current time
+  starttime = perf_counter()
+
+  # Execute function to measure performance
+  args[0](args[1])
+
+  # Get performance of function
+  return get_performance(starttime)
+
+length_of_range = 1_000_000
+
+print(get_functon_performance(upto_for, length_of_range))
+print(get_functon_performance(upto_sum, length_of_range))
