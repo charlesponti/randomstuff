37e5fbaac7a721a2f09d995dbdb545f786d65f26
Charles Ponti <cj@ponti.io>
2020-01-22 07:39:55 -0500

    wip

--- lessons/tests/test_person.py
+++ lessons/tests/test_person.py
@@ -1,5 +1,8 @@
 from lessons.classes import Person, SpecialPerson
 
+def setup_function():
+    print("setting up state...")
+
 def test_person():
     person = Person(name="John", age=35, job="Python Developer")
     assert person.get() == {"name": "John", "age": 35}
@@ -9,3 +12,6 @@ def test_special_person():
     assert person.get() == {"name": "John Doe", "age": 35, "type": "special"}
     assert person.email == "john_doe@email.com"
     assert person.get_display() == "John Doe is 35 and works as a Python Developer"
+
+def teardown_function():
+    print("tearing down state...")
