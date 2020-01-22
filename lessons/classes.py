a636b9daecf081a0b539bcae60bb7ad984d39772
Charles Ponti <cj@ponti.io>
2020-01-21 22:29:46 -0500

    update classes lesson and add test

--- lessons/classes.py
+++ lessons/classes.py
@@ -2,51 +2,30 @@
 # Example file for working with classes
 #
 class Person:
-    def __init__(self, name: str, age: int, hair: str, **kwargs):
+    def __init__(self, name: str, age: int, job: str, **kwargs):
         self.name = name
         self.age = age
-        self.hair = hair
+        self.job = job
 
     def get(self):
-        return {"name": self.name, "age": self.age, "hair": self.hair}
+        return {
+          "name": self.name,
+          "age": self.age
+        }
 
-class myClass():
-  def method1(self):
-    print("my class method1")
+    @property
+    def email(self):
+      return f"{self.name.lower().replace(' ', '_')}@email.com"
 
-  def method2(self, someString):
-    print(f"my class method2 - {someString}")
+class SpecialPerson(Person):
+  type: str
 
-class myOtherClass(myClass):
-  def method1(self):
-    myClass.method1(self)
-    print("myOtherClass method1")
-
-  def method2(self, someString):
-    super().method2("cats")
-    # print(f"myOtherClass - {someString}")
-
-class anotherClass():
-  def __init__(self, name, age, job):
-    self.name = name
-    self.age = age
-    self.job = job
+  def get(self):
+    return {
+      "name": self.name,
+      "age": self.age,
+      "type": "special"
+    }
 
   def get_display(self):
-    return self.name + " is " + str(self.age) + " and works as a " + self.job
-
-def main():
-  class1 = myClass()
-  class1.method1()
-  class1.method2("cats")
-
-  class2 = myOtherClass()
-  class2.method1()
-  class2.method2("cats")
-
-  class3 = anotherClass("John Doe", 35, "Python Developer")
-  print(class3.name, class3.age, class3.job)
-  print(class3.get_display())
-
-if __name__ == "__main__":
-  main()
+    return f"{self.name} is {str(self.age)} and works as a {self.job}"
---
+++ lessons/tests/test_person.py
@@ -0,0 +1,11 @@
+from lessons.classes import Person, SpecialPerson
+
+def test_person():
+    person = Person(name="John", age=35, job="Python Developer")
+    assert person.get() == {"name": "John", "age": 35}
+
+def test_special_person():
+    person = SpecialPerson(name="John Doe", age=35, job="Python Developer")
+    assert person.get() == {"name": "John Doe", "age": 35, "type": "special"}
+    assert person.email == "john_doe@email.com"
+    assert person.get_display() == "John Doe is 35 and works as a Python Developer"
