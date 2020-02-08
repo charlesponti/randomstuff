b2c21b02897f083b853376a356fb5d3bea7f664a
Charlie <cj@ponti.io>
2020-02-08 17:40:32 -0500

    (tests) add additional test for linked_list

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,6 +1,16 @@
 from problems.linked_list import Node
 
 
+def test_get_length():
+    node = Node(4)
+    node.add_to_tail(5)
+    node.add_to_tail(6)
+    assert node.get_length() == 3
+    node.add_to_tail(5)
+    node.add_to_tail(6)
+    assert node.get_length() == 5
+
+
 def test_add_to_tail():
     node = Node(4)
     node.add_to_tail(5)
