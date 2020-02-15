ff081b04619d0bd4c25380f5a5f7815046786f83
Charles Ponti <cj@ponti.io>
2020-02-15 21:58:01 +0000

    all the things

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -2,17 +2,38 @@ from datetime import timedelta
 
 from models.schedule import Schedule, ScheduleItem
 
+global schedule
+global running
+
+schedule = None
+running = None
+
+
+def setup():
+    schedule = Schedule()
+    running = ScheduleItem(name="run", time_length=0.5)
+
 
 def test_schedule_item():
-    schedule_item = ScheduleItem(name="run", time_length=0.5)
-    assert schedule_item.name == "run"
-    assert schedule_item.time_length == 0.5
+    assert running.name == "run"
+    assert running.time_length == 0.5
 
 
 def test_schedule_add_to_daily():
-    schedule = Schedule()
     running = ScheduleItem(name="run", time_length=0.5)
     reading = ScheduleItem(name="reading", time_length=1)
     schedule.add_to_daily(running)
-    schedule.add_to_daily(running)
+    schedule.add_to_daily(reading)
     assert len(schedule.daily) == 2
+
+
+def test_schedule_add_to_daily_no_duplicates():
+    schedule = Schedule()
+    running = ScheduleItem(name="run", time_length=0.5)
+    schedule.add_to_daily(running)
+    schedule.add_to_daily(running)
+    assert len(schedule.daily) == 1
+
+
+def teardown():
+    print("finished")
