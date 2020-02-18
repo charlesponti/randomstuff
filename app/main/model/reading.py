60d4b4ec8c855e5277bccb36a69c4fc309b82474
Charles Ponti <cj@ponti.io>
2020-02-18 19:18:02 +0000

    (reading) Add title to reading models

--- app/main/model/reading.py
+++ app/main/model/reading.py
@@ -1,17 +1,19 @@
 class ReadingItem(object):
     word_length: int
     content: str
+    title: str
 
-    def __init__(self, *args, word_length=0, content: str = None):
+    def __init__(self, *args, word_length=0, content: str = None, title: str = None):
         self.word_length = word_length
         self.content = content
+        self.title = title
 
 
 class Article(ReadingItem):
-    def __init__(self, *args, word_length=0, content=None):
-        super().__init__(*args, word_length=word_length, content=content)
+    def __init__(self, *args, word_length=0, content=None, title: None):
+        super().__init__(*args, word_length=word_length, content=content, title=title)
 
 
 class Book(ReadingItem):
-    def __init__(self, *args, word_length=0, content=None):
-        super().__init__(*args, word_length=word_length, content=content)
+    def __init__(self, *args, word_length=0, content=None, title: None):
+        super().__init__(*args, word_length=word_length, content=content, title=title)
--- tests/test_reading.py
+++ tests/test_reading.py
@@ -11,3 +11,11 @@ def article():
 @pytest.fixture
 def book():
     return Book(title="All Quiet on the Western Front", word_length=75000)
+
+
+@pytest.mark.parametrize("title,no_title", [("Foo Bar", None)])
+def test_articles(title, no_title):
+    book_with_title = Book(title=title)
+    book_with_no_title = Book(title=no_title)
+    assert book_with_no_title.title == None
+    assert book_with_title.title == title
