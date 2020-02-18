3855ba3222ffd0dffe0d42d586f1207c80e6af53
Charles Ponti <cj@ponti.io>
2020-02-18 19:23:33 +0000

    (reading) fix title requirement

--- app/main/model/reading.py
+++ app/main/model/reading.py
@@ -10,10 +10,10 @@ class ReadingItem(object):
 
 
 class Article(ReadingItem):
-    def __init__(self, *args, word_length=0, content=None, title: None):
+    def __init__(self, *args, word_length=0, content=None, title=None):
         super().__init__(*args, word_length=word_length, content=content, title=title)
 
 
 class Book(ReadingItem):
-    def __init__(self, *args, word_length=0, content=None, title: None):
+    def __init__(self, *args, word_length=0, content=None, title=None):
         super().__init__(*args, word_length=word_length, content=content, title=title)
