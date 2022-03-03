b8a4e129ccfce43bdd955788d82d48f8679e34e6
charlesponti <cj@ponti.io>
2022-03-02 16:49:10 -0800

    updatezzz

---
+++ src/life-time.ts
@@ -0,0 +1,23 @@
+/**
+ * ## Lifestyle Calculator
+ * Long ago, I pondered how much could I possibly accomplish in the proceeding three months if all
+ * time were maximally utilized?
+ */
+
+/**
+ * @description Activities linked to the maintainance and improved of the body.
+ */
+export const body = {
+  cooking: '2 H per day',
+  sleep: '7 H per day',
+  exercise: '45 M per day',
+  yoga: '1 H per week',
+};
+
+/**
+ * @description Activities linked to the maintainance and improved of the mind.
+ */
+export const mind = {
+  reading: {cost: '1 H', interval: 'daily', score: 5},
+  writing: {cost: '1 H', interval: 'weekly', score: 2},
+};
