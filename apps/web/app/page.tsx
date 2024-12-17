2206e471fa48a89f5c3d8364ec9b8bdde75b5ec9
Charles Ponti <cj@ponti.io>
2024-12-17 12:17:07 -0800

    feat(apps/web): upgrade docs to latest nextjs

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <header className='p-4 border-[#ebebeb] border-b-[1px]'>
+      <p className="text-2xl font-bold">hiragana (common)</p>
+    </header>
+  );
+}
