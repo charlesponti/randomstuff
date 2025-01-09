7dde71c66e1fd88bf160dd2ba5496e012ba5dec8
Charles Ponti <cj@ponti.io>
2025-01-09 11:42:03 -0800

    refactor(music-maker): remove unused venue types and related utilities

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,6 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 
-import venueApi from "./music-maker/venue";
-
 type Bindings = {
   ENV: string;
   API_ORIGIN: string;
@@ -10,22 +8,17 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>();
 
-app.use(
-  "api/*",
-  async (c, next) => {
-    cors({
-      origin: c.env.API_ORIGIN,
-      allowMethods: ["GET", "POST"],
-      allowHeaders: ["Content-Type"],
-      maxAge: 600,
-    });
-  },
-);
+app.use("api/*", async (c, next) => {
+  cors({
+    origin: c.env.API_ORIGIN,
+    allowMethods: ["GET", "POST"],
+    allowHeaders: ["Content-Type"],
+    maxAge: 600,
+  });
+});
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
-
-app.route("/music-maker", venueApi);
 
 export default app;
